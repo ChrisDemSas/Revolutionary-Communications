1a35dbe285dd2c7337f39383dc04db50879ee142
ChrisDemSas <christopher.sastropranoto@gmail.com>
2024-08-06 18:29:11 +0800

    Finished testing predibase

--- tests/etl_tests.py
+++ tests/etl_tests.py
@@ -3,19 +3,21 @@ import sqlite3
 import filepaths as fp
 import sys
 sys.path.append(fp.PREDIBASE_FILEPATH)
-from predibase import PredibaseSentiment
+from predibase_sentiment import PredibaseSentiment
 import datetime
 
 def test_predibase() -> pd.DataFrame:
     """Tests a predibase instance to create a sentiment."""
 
+    API_TOKEN = ''
+
     data = {
-        'feedback': ['The tour group was very respectful to our culture.'],
+        'feedback': ['The tour group was very disrespectful to our culture.'],
         'time': [datetime.datetime.now()],
         'category': ['culture']
     }
 
-    predibase = PredibaseSentiment('pb_2lVSWVmcVQGkFuKPuQDvIQ', 
+    predibase = PredibaseSentiment(API_TOKEN, 
                           'review-sentiment-model/3')
 
     data = predibase.sentiment(data)
